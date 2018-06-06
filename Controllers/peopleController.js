19f6410495f03c295f8cdcc6bc02ac4d8f7ad719
balen dezai <bade@pc-tec.local>
2018-06-06 10:53:41 +0200

    Fixed minor typos and mistakes in conroller

--- Controllers/peopleController.js
+++ Controllers/peopleController.js
@@ -1,64 +1,64 @@
-import Person from '../models/person';
 import bookControllerDebug from 'debug';
-import person from '../models/person';
+import Person from '../models/person';
+
 const debug = bookControllerDebug('app:peopleController');
 
 const peopleController = {
   FindResource: async (req, res) => {
     try {
-      const foundPeople = await Person.find(req.query).populate('_Job').populate('_Wallet').populate('_Work');
+      const foundPeople = await Person.find(req.query).populat0e('_Job').populate('_Wallet').populate('_Work');
       if (foundPeople.length > 0) {
         res.json(foundPeople);
       } else {
-        res.status(204).send("No Data Found.");
+        res.status(204).send('No Data Found.');
       }
     } catch (error) {
       debug(error);
       res.status(204).send("Error Happened, couldn't find resource.");
     }
   },
+
   FindResourceById: async (req, res) => {
     try {
       const foundPerson = await Person.findById(req.params.id);
       res.json(foundPerson);
     } catch (error) {
-      debug(error),
-        res.status(204).send("No such resource exists");
+      debug(error)
+      res.status(204).send('No such resource exists');
     }
   },
+
   CreateResource: async (req, res) => {
     try {
-      newCreatedPerson = await Person.create(req.body);
+      const newCreatedPerson = await Person.create(req.body);
       await Person.update(newCreatedPerson);
-      res.status(201).send("New Resource Added");
+      res.status(201).send('New Resource Added');
     } catch (error) {
       debug(error);
-      res.sendStatus(500).send("Error processing the request");
+      res.sendStatus(500).send('Error processing the request');
     }
   },
 
   UpdateResource: async (req, res) => {
     try {
-      debug("I WAS HERE")
       await Person.findByIdAndUpdate(req.params.id, req.body)
-      res.status(200).send("Resource Updated");
+      res.status(200).send('Resource Updated');
     } catch (error) {
       debug(error);
-      res.sendStatus(500).send("Error processing the request");
+      res.sendStatus(500).send('Error processing the request');
     }
-
   },
 
   DeleteResource: async (req, res) => {
     try {
       await Person.remove({ _id: req.params.id });
-      res.status(200).send("Resource Deleted");
+      res.status(200).send('Resource Deleted');
     } catch (error) {
       debug(error);
-      res.sendStatus(500).send("Error processing the request")
+      res.sendStatus(500).send('Error processing the request');
     }
-  }
-}
+  },
+};
 
 export default peopleController;
 
